79bf2c53e1a1875cde6fc1a38ee50d8e63d5baca
RobomrFox <avikp2002@gmail.com>
2025-11-29 21:27:34 -0700

    Route

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat', include('chat.urls')),
+    path('chat/', include('chat.urls')),
     path('accounts/', include('allauth.urls')),
     path('', RedirectView.as_view(url='/chat', permanent=False))
 ]
